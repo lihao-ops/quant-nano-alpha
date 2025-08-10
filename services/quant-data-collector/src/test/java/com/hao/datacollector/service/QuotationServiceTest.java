e85f6b62c163da74955143d060ca6da64f9ef7ae
hli <lihao836184425gmail.com>
2025-08-10 13:05:45 +0800

    202312

--- services/quant-data-collector/src/test/java/com/hao/datacollector/service/QuotationServiceTest.java
+++ services/quant-data-collector/src/test/java/com/hao/datacollector/service/QuotationServiceTest.java
@@ -44,7 +44,7 @@ class QuotationServiceTest {
         List<String> yearTradeDateList = DateUtil.formatLocalDateList(DateCache.Year2023TradeDateList, DateTimeFormatConstants.EIGHT_DIGIT_DATE_FORMAT);
         //从当年已转档的最大日期(包含),并且剔除最大日期已经转档过的windCode,继续开始转档
         String maxEndDate = quotationMapper.getMaxHistoryTrendEndDate("2023");
-//        String maxEndDate = "20231009";
+//        String maxEndDate = "20231101";
         List<String> completedWindCodes = quotationMapper.getCompletedWindCodes(maxEndDate);
         int tradeDateIndexOf = yearTradeDateList.indexOf(maxEndDate);
         int batchSize = 100;
@@ -67,8 +67,8 @@ class QuotationServiceTest {
     private void transferOneDay(List<String> yearTradeDateList, List<String> windCodes, int batchSize) {
         int totalSize = windCodes.size();
         for (String tradeDate : yearTradeDateList) {
-            if (tradeDate.contains("202311")) {
-                throw new RuntimeException("202311!!!!");
+            if (tradeDate.contains("202312")) {
+                throw new RuntimeException("202312!!!!");
             }
             for (int i = 0; i < totalSize; i += batchSize) {
                 List<String> subList = windCodes.subList(i, Math.min(i + batchSize, totalSize));
