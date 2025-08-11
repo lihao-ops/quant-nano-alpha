5ee54a0fd21441d07b6799325a87c78e6c361125
hli <lihao836184425gmail.com>
2025-08-11 17:58:03 +0800

    子项目新增nacos日志配置

--- services/quant-xxl-job/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ services/quant-xxl-job/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -2,10 +2,12 @@ package com.xxl.job.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author xuxueli 2018-10-28 00:38:13
  */
+@EnableDiscoveryClient//开启服务发现功能
 @SpringBootApplication
 public class XxlJobAdminApplication {
 
