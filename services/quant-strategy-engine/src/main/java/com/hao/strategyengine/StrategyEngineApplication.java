d12326f68e1ee2980f55d39e357709a634ca45ec
hli <lihao836184425gmail.com>
2025-08-12 20:15:10 +0800

    创建strategy-engine子模块并接入nacos配置中心

---
+++ services/quant-strategy-engine/src/main/java/com/hao/strategyengine/StrategyEngineApplication.java
@@ -0,0 +1,49 @@
+package com.hao.strategyengine;
+
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Slf4j
+@MapperScan("com.hao.strategyengine.dal.dao")
+@EnableDiscoveryClient//开启服务发现功能
+@SpringBootApplication
+public class StrategyEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrategyEngineApplication.class, args);
+    }
+
+    //1.项目启动就监听配置文件变化
+    //2.发生变化后拿到变化的内容
+    //3，发送提醒邮件
+    @Bean
+    ApplicationRunner applicationRunner(NacosConfigManager nacosConfigManager) {
+        return args -> {
+            ConfigService configService = nacosConfigManager.getConfigService();
+            configService.addListener("application-dev.yml", "quant-strategy-engine", new Listener() {
+                @Override
+                public Executor getExecutor() {
+                    return Executors.newFixedThreadPool(2);
+                }
+
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    log.info("nacosConfig_conversionInfo_listener_info={}", configInfo);
+                    log.info("to email!");
+                }
+            });
+            log.info("start_nacosConfig_conversionInfo_listener!");
+        };
+    }
+}
---
+++ services/quant-strategy-engine/src/test/java/com/hao/strategyengine/StrategyEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hao.strategyengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
