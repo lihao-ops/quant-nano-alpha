e68fb03b85acfacd5c7933474f52b2cc9d0ed8c4
hli <lihao836184425gmail.com>
2025-08-24 21:23:35 +0800

    大盘指数监控controller

---
+++ services/quant-risk-control/src/main/java/com/hao/riskcontrol/web/controller/MarketIndexController.java
@@ -0,0 +1,11 @@
+package com.hao.riskcontrol.web.controller;
+
+/**
+ * @author hli
+ * @program: quant-nano-alpha
+ * @Date 2025-08-24 21:21:18
+ * @description: 大盘指数监控controller
+ * 大盘指数等信息是中国A股市场最重要、最具代表性的几大指数，它们从不同维度反映了市场的走势。我们可以把它们想象成衡量市场健康状况的“不同温度计”。
+ */
+public class MarketIndexController {
+}
