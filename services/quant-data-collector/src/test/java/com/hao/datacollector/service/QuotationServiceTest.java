da7eafdae362806fbe41a3cfb13cdb9a395dd840
hli <lihao836184425gmail.com>
2025-09-04 16:57:34 +0800

    20250701

--- services/quant-data-collector/src/test/java/com/hao/datacollector/service/QuotationServiceTest.java
+++ services/quant-data-collector/src/test/java/com/hao/datacollector/service/QuotationServiceTest.java
@@ -41,10 +41,10 @@ class QuotationServiceTest {
     @Test
     void transferQuotationHistoryTrend() {
         List<String> allWindCodeList = new ArrayList<>(StockCache.allWindCode);
-        List<String> yearTradeDateList = DateUtil.formatLocalDateList(DateCache.Year2020TradeDateList, DateTimeFormatConstants.EIGHT_DIGIT_DATE_FORMAT);
+        List<String> yearTradeDateList = DateUtil.formatLocalDateList(DateCache.CurrentYearTradeDateList, DateTimeFormatConstants.EIGHT_DIGIT_DATE_FORMAT);
         //从当年已转档的最大日期(包含),并且剔除最大日期已经转档过的windCode,继续开始转档
-        String maxEndDate = quotationMapper.getMaxHistoryTrendEndDate("2020");
-//        String maxEndDate = "20201102";
+        String maxEndDate = quotationMapper.getMaxHistoryTrendEndDate("2025");
+//        String maxEndDate = "20250701";
         List<String> completedWindCodes = quotationMapper.getCompletedWindCodes(maxEndDate);
         int tradeDateIndexOf = yearTradeDateList.indexOf(maxEndDate);
         int batchSize = 100;
@@ -67,9 +67,9 @@ class QuotationServiceTest {
     private void transferOneDay(List<String> yearTradeDateList, List<String> windCodes, int batchSize) {
         int totalSize = windCodes.size();
         for (String tradeDate : yearTradeDateList) {
-            if (tradeDate.contains("202012")) {
+            if (tradeDate.contains("202508")) {
                 log.error("out!,tradeDate={}", tradeDate);
-                throw new RuntimeException("202012!!!!");
+                throw new RuntimeException("202508!!!!");
             }
             for (int i = 0; i < totalSize; i += batchSize) {
                 List<String> subList = windCodes.subList(i, Math.min(i + batchSize, totalSize));
